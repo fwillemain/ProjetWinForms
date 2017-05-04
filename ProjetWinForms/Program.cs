4e6192e4b93d46ce5f0298bfaf3387fdfd5f7152
vportemer <vportemer@VPORTEMER17-DE>
2017-05-04 09:40:47 +0200

    ss

--- ProjetWinForms/Program.cs
+++ ProjetWinForms/Program.cs
@@ -18,6 +18,8 @@ namespace ProjetWinForms
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             // Je fais un test
+
+            char    ;
         }
     }
 }
